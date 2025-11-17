b596a8bbcc4211d381c2f6687008b8376f8a4cdc
AngelSg35395 <minecrafteroangel77@gmail.com>
2025-11-17 15:38:10 -0600

    v1.0

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors({
-    origin: '*', // Change
+    origin: 'https://test-six-omega-10.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }))
-// app.use(securityOrigin) Change
+app.use(securityOrigin)
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
