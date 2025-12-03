d6b8ae45835bfb281bdb2a7fbd0c6c6cd8f753ad
AngelSg35395 <minecrafteroangel77@gmail.com>
2025-12-02 22:31:46 -0600

    pre relese candidate 1.99

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -5,7 +5,7 @@ export const getProductsType = async (req, res) => {
     try {
         const type = req.params.typePath
 
-        let query = supabase.from('products').select('*').order('product_id', { ascending: true })
+        let query = supabase.from('products').select('*').order('product_id', { ascending: false })
 
         if (type === 'initialProducts') {
             query = query.is('initially_show', true)
